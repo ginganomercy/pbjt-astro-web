46e9980c0a2cea2c113588057fd48fb7d80698ab
ginganomercy <rafly1955@gmail.com>
2026-09-03 07:16:46 +0700

    Refactor: extract MainLayout and improve mobile responsiveness

--- push.ps1
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit for PBJT Astro Headless"
-git branch -M main
-git remote add origin https://github.com/ginganomercy/pbjt-astro-web.git
-git push -u origin main
